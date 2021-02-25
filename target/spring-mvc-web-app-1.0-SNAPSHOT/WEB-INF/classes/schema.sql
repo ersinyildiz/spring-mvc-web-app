300cba837b85361fbe7d159ae33fe450d482bb1c
ersinyildiz <ahmetersinyildiz@gmail.com>
2021-02-25 21:37:30 +0300

    bug fix

---
+++ target/spring-mvc-web-app-1.0-SNAPSHOT/WEB-INF/classes/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE person (
+    id INT(11) PRIMARY KEY NOT NULL,
+    firstName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    bloodGroup VARCHAR(45) NOT NULL,
+    phoneNumber VARCHAR(45) NOT NULL,
+    address VARCHAR(128) NOT NULL
+);
